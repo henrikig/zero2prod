049987084bc23eab94f1d2d50bb6292fb2d47ba3
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2023-01-08 21:17:42 +0100

    :sparkles: add first endpoints, tests, db and configuration

---
+++ migrations/20230108192837_create_subscription_table.sql
@@ -0,0 +1,8 @@
+-- Create subscriptions table
+CREATE TABLE subscriptions(
+  id uuid NOT NULL,
+  PRIMARY KEY (id),
+  email TEXT NOT NULL UNIQUE,
+  name TEXT NOT NULL UNIQUE,
+  subscribed_at timestamptz NOT NULL
+)
