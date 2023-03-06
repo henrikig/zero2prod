d849a7330ea51eb5fba872d6cc2a62fa49910d0f
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2023-03-06 21:45:00 +0100

    :sparkles: login form

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
 use crate::{
     configuration::{DatabaseSettings, Settings},
     email_client::EmailClient,
-    routes::{confirm, health_check, publish_newsletter, subscribe},
+    routes::{confirm, health_check, home, login, login_form, publish_newsletter, subscribe},
 };
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
@@ -77,6 +77,9 @@ pub fn run(
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .route("/", web::get().to(home))
+            .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .route("health_check", web::get().to(health_check))
             .route("subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
