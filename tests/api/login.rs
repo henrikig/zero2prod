f7d8f112f2ddf3ff63a8620e7d17b70913104e05
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2023-04-10 22:11:56 +0200

    add login tests

---
+++ tests/api/login.rs
@@ -0,0 +1,23 @@
+use crate::helpers::{assert_is_redirect_response, spawn_app};
+
+#[tokio::test]
+async fn an_error_flash_message_is_set_on_failure() {
+    let app = spawn_app().await;
+
+    let login = serde_json::json!({
+        "username": "random-username",
+        "password": "random-password",
+    });
+    let response = app.post_login(&login).await;
+
+    assert_is_redirect_response(&response, "/login");
+
+    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
+    assert_eq!(flash_cookie.value(), "Authentication failed");
+
+    let html_page = app.get_login_html().await;
+    assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
+
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
+}
